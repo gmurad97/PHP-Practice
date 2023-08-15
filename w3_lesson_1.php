e3a1f3f51fe0ff59b326a936b5c1ee84d266e624
gmurad97 <murad.dev@bk.ru>
2023-08-15 05:32:28 +0400

    finished lesson 1

---
+++ w3_lesson_1.php
@@ -0,0 +1,101 @@
+<?php
+
+//CONSTANT EXAMPLE
+
+/* define("JUNIOR","JUNIOR FULLSTACK WEB DEVELOPER");
+define("MIDDLE","MIDDLE FULLSTACK WEB DEVELOPER");
+define("SENIOR","SENIOR FULLSTACK WEB DEVELOPER");
+
+echo JUNIOR . "<br>";
+echo MIDDLE . "<br>";
+echo SENIOR . "<br>"; */
+
+//==================================================
+
+/* define("CARS",["Mercedes","BMW","Audi"]);
+echo CARS[0] . "<br>";
+echo CARS[1] . "<br>";
+echo CARS[2] . "<br>"; */
+
+//==================================================
+
+/* define("CARS", ["Mercedes" => "S600", "BMW" => "M5", "Audi" => "RS6"]);
+echo CARS["Mercedes"] . "<br>";
+echo CARS["BMW"] . "<br>";
+echo CARS["Audi"] . "<br>"; */
+
+/* //==================================================
+//IF ELSEIF ELSE EXAMPLE 
+
+
+$condition = 10 > 5;
+
+//==================================================
+
+if ($condition) {
+    //expression if
+}
+
+//==================================================
+
+if ($condition) {
+    //expression if
+} else {
+    //expression else
+}
+
+//==================================================
+
+if ($condition) {
+    //expression if
+} else if (!$condition) {
+    //expression elseif
+} else {
+    //expression else
+}
+
+//==================================================
+
+if ($condition) :
+//expression if
+endif;
+
+//==================================================
+
+if ($condition) :
+//expression if
+else :
+//expression else
+endif;
+
+//==================================================
+
+if ($condition) :
+//expression if
+elseif (!$condition) :
+//expression elseif
+else :
+//expression else
+endif;
+
+//================================================== */
+
+//SWITCH CASE
+
+/* $color = "red";
+
+switch ($color) {
+    case "red":
+        echo "red";
+        break;
+    case "green":
+        echo "green";
+        break;
+    case "blue":
+        echo "blue";
+        break;
+    default:
+        echo "undefined color";
+        break;
+}
+ */

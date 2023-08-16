2ce7a6bb632f5cbc6ef4285bc8f6245cabfca913
gmurad97 <murad.dev@bk.ru>
2023-08-17 01:56:23 +0400

    finished w3_lesson_array

---
+++ w3_lesson_array.php
@@ -0,0 +1,56 @@
+<?php
+
+//CREATE ARRAY -> []
+
+/* $arr_ndx = [1, 2, 3]; // indexed array
+
+// associative array
+$arr_assoc = [
+    "brand" => "Mercedes-Benz",
+    "model" => "S600 Brabus",
+    "year" => 2023,
+    "hp" => 1024,
+];
+
+//multidimensional arrays
+$arr_md = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    [
+        ["7", "8", "9"]
+    ]
+
+]; */
+
+//CREATE ARRAY array();
+
+/* $arr_ndx = array(1, 2, 3); // indexed array
+
+// associative array
+$arr_assoc = array(
+    "brand" => "Mercedes-Benz",
+    "model" => "S600 Brabus",
+    "year" => 2023,
+    "hp" => 1024,
+);
+
+//multidimensional arrays
+$arr_md = array(
+    array("1", "2", "3"),
+    array("4", "5", "6"),
+    array(
+        array("7", "8", "9")
+        )
+
+); */
+
+//SORTING ARRAY
+
+/* sort();//Sort ASC
+rsort();//Sort DESC */
+
+/* asort();//Sort ASC assoc array value
+arsort();//Sort DESC assoc array value
+
+ksort();//Sort ASC assoc array key
+krsort();//Sort DESC assoc array key */

dee09c0cc1d8cdabebd421cbfc51f9ac1599f7b8
gmurad97 <murad.dev@bk.ru>
2023-08-18 01:55:27 +0400

    finished w3_lesson_regex

---
+++ w3_lesson_regex.php
@@ -0,0 +1,17 @@
+<?php
+
+/* $regex_pattern = "/(?<=date\": \").*(?=\")/i";
+$subject = "{
+    \"time\": \"03:53:25 AM\",
+    \"milliseconds_since_epoch\": 1362196405309,
+    \"date\": \"03-02-2013\",
+    \"time\": \"03:53:25 AM\",
+    \"milliseconds_since_epoch\": 1362196405309,
+    \"date\": \"03-02-2013\"
+            }";
+$matches = []; */
+
+/* preg_match($regex_pattern, $subject, $matches); // ONLY FIRST MATCH
+preg_match_all($regex_pattern, $subject, $matches); // ALL MATCHES */
+
+/* print_r($matches); */

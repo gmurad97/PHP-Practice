9e19ae731bf85c9a65befae039a2942252802a54
gmurad97 <murad.dev@bk.ru>
2023-08-19 04:00:38 +0400

    sorting file

---
+++ PHP Docs/d_array.php
@@ -0,0 +1,17 @@
+<?php
+
+//DESTRUCT ARRAY
+
+/* $arr = ["Mercedes-Benz","S600","5.5"]; */
+
+//EXAMPLE #1 DESTRUCT TYPE
+
+/* [$brand, $model, $engine] = $arr;
+echo $brand . " " . $model . " " . $engine . " v8 Engine"; */
+
+//EXAMPLE #2 DESTRUCT TYPE
+
+/* list($brand, $model, $engine) = $arr;
+echo $brand . " " . $model . " " . $engine . " v8 Engine"; */
+
+//==================================================

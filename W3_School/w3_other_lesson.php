850533c5c6d2da6c21ded8607f45ed55034ee062
gmurad97 <murad.dev@bk.ru>
2023-08-22 18:58:51 +0400

    regex & filter_var

---
+++ W3_School/w3_other_lesson.php
@@ -0,0 +1,12 @@
+<?php
+
+/* $input_email = "murad.dev@bk.ru";
+
+$isEmail = filter_var($input_email,FILTER_VALIDATE_EMAIL);
+
+echo "INPUT:\t".$input_email."<br>";
+echo "isEmail:\t" . ($isEmail ? "true":"false"); */
+
+
+/* date_default_timezone_set("Asian/Baku");
+echo "The time is " . date("h:i:sa"); */

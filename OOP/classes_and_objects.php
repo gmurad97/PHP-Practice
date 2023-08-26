8221a4605e239e6deb2a385594200c7a1467f6a5
gmurad97 <murad.dev@bk.ru>
2023-08-26 04:50:32 +0400

    oop classes objects and (contructor & destructor)

---
+++ OOP/classes_and_objects.php
@@ -0,0 +1,21 @@
+<?php
+
+/* class Fruit{
+    protected $name;
+    public $color;
+
+    public function set_fruit($name,$color){
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    public function get_fruit(){
+        return $this->name ."<br>". $this->color;
+    }
+}
+
+
+$kiwi = new Fruit();
+$kiwi->set_fruit("kiwi","brown");
+
+echo $kiwi->name; */
---
+++ OOP/w3_contructor.php
@@ -0,0 +1,21 @@
+<?php
+
+/* class Auto
+{
+    private $model, $brand;
+    public function __construct($brand, $model)
+    {
+        $this->model = $model;
+        $this->brand = $brand;
+    }
+
+    public function getFullName()
+    {
+        return $this->brand . "<br>" . $this->model;
+    }
+}
+
+
+$mercedes = new Auto("Mercedes-Benz", "S600");
+
+echo $mercedes->getFullName(); */

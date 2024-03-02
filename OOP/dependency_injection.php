980e8bf74513b4e31f2c8c60b16d37027a6bb612
gmurad97 <murad.dev@bk.ru>
2024-03-03 01:28:49 +0400

    dependency injection (DI)

---
+++ OOP/dependency_injection.php
@@ -0,0 +1,29 @@
+<?php
+
+/* class Dependency
+{
+    public function helloMessage(): string
+    {
+        return "Hello";
+    }
+}
+class MyClass
+{
+    private $dependency;
+
+    public function __construct(Dependency $dependency)
+    {
+        $this->dependency = $dependency;
+    }
+
+    public function showMessage(): string
+    {
+        return $this->dependency->helloMessage();
+    }
+}
+
+$dependency = new Dependency();
+$myClass = new MyClass($dependency);
+
+print_r($myClass->showMessage()); */
+
